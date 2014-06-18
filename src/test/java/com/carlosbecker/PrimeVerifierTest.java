b25b3fdfd6668e4e22316c771b85bd225a7536df
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-06-18 00:26:01 -0300

    Update PrimeVerifierTest.java

--- src/test/java/com/carlosbecker/PrimeVerifierTest.java
+++ src/test/java/com/carlosbecker/PrimeVerifierTest.java
@@ -6,9 +6,6 @@ import org.junit.Test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-/**
- * @author carlos
- */
 public class PrimeVerifierTest {
 	private PrimeVerifier prime;
 
